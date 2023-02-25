b1901a0956af119ba815d9a04ae8472730fd10b9
CodeKrakken <newholm1@hotmail.com>
2023-02-25 18:38:13 +0000

    Add useEffect to series formatting in MarketGraph

--- src/components/MarketGraph/MarketGraph.tsx
+++ src/components/MarketGraph/MarketGraph.tsx
@@ -1,5 +1,6 @@
 import Chart from 'react-apexcharts';
 import { indexedFrame } from 'server';
+import { useEffect } from 'react';
 
 const MarketGraph = ({ history }: { history: any }) => {
 
@@ -22,17 +23,21 @@ const MarketGraph = ({ history }: { history: any }) => {
     },
   };
 
-  const series = [
-    {
-      data: history.map((candle: indexedFrame) => [
-        candle.time,
-        candle.open,
-        candle.high,
-        candle.low,
-        candle.close,
-      ]),
-    },
-  ];
+  let series
+
+  useEffect(() => {
+    series = [
+      {
+        data: history.map((candle: indexedFrame) => [
+          candle.time,
+          candle.open,
+          candle.high,
+          candle.low,
+          candle.close,
+        ]),
+      },
+    ];
+  }, [])
 
   return (
     <Chart options={options} series={series} type="candlestick" height={350} />
