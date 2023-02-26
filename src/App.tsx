489b573f792c79a17d142a1045baabf8b0b9d749
CodeKrakken <newholm1@hotmail.com>
2023-02-26 13:12:33 +0000

    Argh is this right one

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ export default function App() {
     {transactionLog.length ? <TransactionLog log={transactionLog} /> : null}
     <br />
     {
-      wallet?.data?.currentMarket?.histories
+      wallet?.data?.currentMarket?.histories?.seconds
       ? <MarketGraph history={wallet.data.currentMarket.histories.seconds} />
       : null
     }
