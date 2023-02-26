dc1dd02aff016920f92f056b93f8ece0920a7ae9
CodeKrakken <newholm1@hotmail.com>
2023-02-26 12:30:54 +0000

    Reduce huge conditional

--- src/App.tsx
+++ src/App.tsx
@@ -45,9 +45,9 @@ export default function App() {
       // wallet &&
       // Object.keys(wallet).length &&
       wallet.data &&
-      wallet.data.currentMarket 
+      wallet.data.currentMarket &&
+      wallet.data.currentMarket.histories 
       // &&
-      // wallet.data.currentMarket.histories &&
       // wallet.data.currentMarket.histories.seconds &&
       // wallet.data.currentMarket.histories.seconds.length 
       ?
