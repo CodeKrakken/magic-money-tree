610e49d6147b29029636f33bb47f62f7192a83a5
CodeKrakken <newholm1@hotmail.com>
2023-03-02 21:03:20 +0000

    Remove redundant code and hopefully unwanted bullet

--- src/components/StringList/StringList.tsx
+++ src/components/StringList/StringList.tsx
@@ -1,22 +1,22 @@
+import './StringList.css'
+
 export default function StringList({
   list, 
-  title,
   attrs
 } : {
   list: string[], 
-  title: string,
+
   attrs?: {
     [key: string]: {
       [key: string]: string
     }
   }
 }) {
-  return <>
-    {/* <h1 {...attrs?.title}>{title}</h1> */}
+  return <ul>
     {
       list.map(item =>
         <li {...attrs?.content}>{item}</li>
       )
     }
-  </>
+  </ul>
 }
