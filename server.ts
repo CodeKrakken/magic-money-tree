932426406d10fdf4c0086d87321ac8da7f1c2732
CodeKrakken <newholm1@hotmail.com>
2023-03-07 21:43:11 +0000

    Remove sell logic to prevent bug at this point

--- server.ts
+++ server.ts
@@ -662,11 +662,11 @@ async function trade(sortedMarkets: market[]) {
       const currentMarket = markets[wallet.data.currentMarket.name]
 
       if (currentMarket.shape as number < 1 || currentMarket.emaRatio as number < 1 || currentMarket.strength as number < 1) {
-        simulatedSellOrder('Bear', currentMarket)
+        // simulatedSellOrder('Bear', currentMarket)
       } else if (!currentMarket) {
         // simulatedSellOrder('No response for current market', markets[wallet.data.currentMarket.name])
       } else if (targetMarket?.name !== currentMarket.name && wallet.coins[wallet.data.baseCoin].dollarPrice >= (wallet.data.prices.targetPrice as number)) { 
-        simulatedSellOrder('New Bull', currentMarket)
+        // simulatedSellOrder('New Bull', currentMarket)
       } else if (!wallet.data.prices.targetPrice || !wallet.data.prices.stopLossPrice) {
         // simulatedSellOrder('Price information undefined', markets[wallet.data.currentMarket.name])
       } else if (wallet.coins[wallet.data.baseCoin].dollarPrice as number < wallet.data.prices.stopLossPrice) {
