33333f8e7ba6d4ca2b5fdd7ac99b663a0d321fbe
CodeKrakken <newholm1@hotmail.com>
2023-03-15 10:09:19 +0000

    Fix scroll?

--- src/components/StringList/StringList.tsx
+++ src/components/StringList/StringList.tsx
@@ -4,7 +4,24 @@ import { stringList } from 'src/App';
 export default function StringList({list} : {list: stringList}) {
 
   return <>
-    <div className="col-container">
+    <div style={{height: '200px', overflow: 'auto'}}>
+      {
+        list?.lines?.map(line => {
+          console.log(line)
+          const columns = line.text
+          return <div className="col-row" {...(line.time && { title: line.time })}>
+            {
+              columns?.map(col => 
+                <div className="col">
+                  {col}
+                </div>
+              )
+            }
+          </div>
+        })
+      }
+    </div>
+    {/* <div className="col-container">
       <div className="col-row">
         {
           list?.headers?.map(item => 
@@ -30,6 +47,6 @@ export default function StringList({list} : {list: stringList}) {
           }
         )
       }
-    </div>
+    </div> */}
   </>
 }
