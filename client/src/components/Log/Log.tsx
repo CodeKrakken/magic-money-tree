f1b05106764b79228f602e15b0ab3c32946abcfa
CodeKrakken <newholm1@hotmail.com>
2026-09-11 12:54:06 +0100

    Simplify Log component

--- client/src/components/Log/Log.tsx
+++ client/src/components/Log/Log.tsx
@@ -1,42 +1,16 @@
-import { useRef, useEffect, useState } from 'react';
-
 const Log = (
   log: string[]
 ) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [shouldScrollToBottom, setShouldScrollToBottom] = useState(true);
 
-  useEffect(() => {
-    if (containerRef.current) {
-      if (shouldScrollToBottom) {
-        containerRef.current.scrollTop = containerRef.current.scrollHeight;
-      }
+  return <div>
+    {
+      log.map((item, i) => (
+        <div key={i}>
+          {item}
+        </div>
+      ))
     }
-  }, [log, shouldScrollToBottom]);
-
-  function handleScroll() {
-    if (containerRef.current) {
-      const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
-      const isAtBottom = scrollHeight - scrollTop === clientHeight;
-      setShouldScrollToBottom(isAtBottom);
-    }
-  }
-
-  return (
-    <div 
-      style={{
-        height: '60vh',
-        overflowY: 'scroll'
-      }} 
-      ref={containerRef} onScroll={handleScroll}
-    >
-      {
-        log.map((item, i) => (
-          <div title={'Record '+(i+1)} key={i}>{item}</div>
-        ))
-      }
-    </div>
-  );
+  </div>
 };
 
 export default Log;
