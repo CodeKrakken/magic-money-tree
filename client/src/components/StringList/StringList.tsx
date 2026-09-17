4ed1088d8dc5bee8d18ab5d980640a48dcccf43d
CodeKrakken <newholm1@hotmail.com>
2026-09-17 16:12:44 +0100

    Space StringList out

--- client/src/components/StringList/StringList.tsx
+++ client/src/components/StringList/StringList.tsx
@@ -5,19 +5,33 @@ type transaction = {
   time: string
 }
 
-export default function StringList({list} : {list: string[]|transaction[]}) {
+export default function StringList({
 
-  function isTransaction(entry: string|transaction): entry is transaction {
+  list
+
+} : {
+
+  list: string[] | transaction[]
+
+}) {
+
+  function isTransaction(entry: string | transaction): entry is transaction {
     return (entry as transaction).time !== undefined;
   }
 
   return <ul>
     {
-      list.map(item => {
-        return isTransaction(item)
-        ? <li title={item.time}>{item.text}</li>
-        : <li>{item}</li>
-      })
+      list.map(item => 
+        isTransaction(item) 
+          ? 
+        <li title={item.time}>
+          {item.text}
+        </li>
+          : 
+        <li>
+          {item}
+        </li>
+      )
     }
   </ul>
 }
