033899ba4cb9123d5222367dc077f8ea21342ee4
CodeKrakken <newholm1@hotmail.com>
2026-09-18 23:42:16 +0100

    Add console.log to check server call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -79,6 +79,8 @@ export default function App() {
         }
 
         const data = await response.json();
+        console.log(data)
+
         if (!cancelled) {
           setPortfolioHistory(data);
         }
